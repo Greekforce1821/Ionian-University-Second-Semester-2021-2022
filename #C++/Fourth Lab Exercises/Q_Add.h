748c56e47e502569e0780c4954d8bcd413d6c163
Spyros Kokotos <33377581+Greekforce1821@users.noreply.github.com>
2022-03-29 19:41:40 +0300

    Create Q_Add.h

---
+++ #C++/Fourth Lab Exercises/Q_Add.h
@@ -0,0 +1,17 @@
+#ifndef MYRANDOM_H
+#define MYRANDOM_H
+#include "MyRandom.h"
+#endif
+#include <string>
+#include <iostream>
+using namespace std;
+
+class Q_Add {
+	private:
+	    static MyRandom mr;
+		int a,b;
+	public:
+		Q_Add(int m);
+		void print_quest() const;
+		int get_Correct() const;
+};
