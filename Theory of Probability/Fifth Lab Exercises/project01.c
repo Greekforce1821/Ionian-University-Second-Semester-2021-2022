62fc4b57112a80caf0b3fb4c1531b5837a8f7162
Spyros Kokotos <33377581+Greekforce1821@users.noreply.github.com>
2022-04-03 21:12:33 +0300

    Create project01.c

---
+++ Theory of Probability/Fifth Lab Exercises/project01.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main(){
+	srand(time(NULL));
+	
+	
+	int i;
+	int j;
+	
+	int counter;
+	int s = 0;
+	int a;
+	
+	int r;
+	
+	for (j=0; j<=100; j++){
+		counter = 0; 
+		//inner loop - i
+		for(i=0; i<=10; i++){
+			r = rand()%2;
+			printf("%d\n", r);
+			a = 0;
+			if (a == 0){
+				if (i <= 3){
+					if (r == 1){
+						counter++;
+					}
+					else{
+						a = 1;
+					}
+				}
+			}
+		}
+		if (counter == 3){
+			s++;
+		}
+		printf("\n");
+	}
+	
+	printf("How many times does the dice got correct? %d\n", s);
+	
+	return 0;
