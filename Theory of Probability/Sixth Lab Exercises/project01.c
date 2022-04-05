d34357b237ecd7978ade373d1d5dab2fdb3aa145
Spyros Kokotos <33377581+Greekforce1821@users.noreply.github.com>
2022-04-06 00:28:26 +0300

    Update project01.c

--- Theory of Probability/Sixth Lab Exercises/project01.c
+++ Theory of Probability/Sixth Lab Exercises/project01.c
@@ -1,24 +1,78 @@
 #include <stdio.h>
-int main() {
-    int n, i;
-    float num[10], sum = 0.0, avg;
+#include <stdlib.h>
+#include <math.h>
 
-    printf("Enter the numbers of elements: ");
-    scanf("%d", &n);
+double mean(double val[10], int n){
+	
+	double result;
+	int i;
+	int sum = 0;
 
-    while (n > 10 || n < 1) {
-        printf("Error! number should in range of (1 to 10).\n");
-        printf("Enter the number again: ");
-        scanf("%d", &n);
-    }
-
-    for (i = 0; i < n; ++i) {
-        printf("%d. Enter number: ", i + 1);
-        scanf("%f", &num[i]);
-        sum += num[i];
-    }
-
-    avg = sum / n;
-    printf("The Average Price of the Children Weight is = %.2f", avg);
-    return 0;
+	for(i=0; i<=10; i++){
+		sum = sum + val[i];
+	}
+	
+	result = sum/n;
+	return result;
+	
 }
+
+double variance(double val[10], int n){
+	
+	double s;
+	int i;
+	int sum = 0;
+	
+	double m;
+	double sqdiff = 0;
+	
+	for (i=0; i<=10; i++){
+		sum = sum + val[i];
+		m = mean(val, 10);
+		
+		sqdiff = sqdiff + (val[i] - m) * (val[i] - m);
+	}
+	
+	
+	s = sqdiff/n;
+	return s;
+}
+
+double stddev(double val[10], int n){
+	
+	double s;
+	
+	s = variance(val, 10);
+	double sdv = sqrt(s);
+	return sdv;
+	
+}
+
+int main({
+	
+	int i;
+	double val[10];
+	double result;
+	int n = 10;
+	
+	
+	double m; 
+	double v; 
+	double sd; 
+	
+	for (i=0; i<=10; i++){
+		val[i] = rand();
+	}
+	
+	m = mean(val, 10);
+	printf("%f\n", m);
+	
+	v = variance(val, 10);
+	printf("%f\n", v);
+	
+	sd = stddev(val, 10);
+	printf("%f\n", sd);
+	
+	return 0;
+}
+
