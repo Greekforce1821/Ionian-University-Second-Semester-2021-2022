b9ef089ff76d4beeab0add66b5c97a1dd70223f9
Spyros Kokotos <33377581+Greekforce1821@users.noreply.github.com>
2022-04-06 16:42:27 +0000

    Add files via upload

---
+++ #C++/Fifth Lab Exercises/Vehicle.h
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+Class Vehicle{
+              private:
+                      int year; // έτος κατασκευής
+              protected:
+                       string number; // πινακίδα κυκλοφορίας
+                       string owner; // κάτοχος ΙΧ
+              public:
+                     Vehicle();
+                     Vehicle(string owner, string number, int year);
+                     void set_owner(string o); // συνάρτηση για την αλλαγή του ιδιοκτήτη
+                     void show(); // συνάρτηση για την εμφάνιση των στοιχείων του οχήματος
+}; 
+                
