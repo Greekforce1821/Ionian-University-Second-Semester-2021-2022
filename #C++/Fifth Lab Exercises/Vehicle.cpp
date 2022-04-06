a2c9759ef6617b024f044254e807d4e7393f0b39
Spyros Kokotos <33377581+Greekforce1821@users.noreply.github.com>
2022-04-06 17:29:24 +0000

    Add files via upload

--- #C++/Fifth Lab Exercises/Vehicle.cpp
+++ #C++/Fifth Lab Exercises/Vehicle.cpp
@@ -6,3 +6,22 @@ Vehicle::Vehicle(){
    
 }
 
+Vehicle::Vehicle(string o, string n, int y){
+
+    owner = o;
+    number = n;
+    year = y;
+    
+
+}
+
+void Vehicle::set_owner(string o){
+
+    owner = o;
+
+}
+
+void Vehicle::show(){
+
+    cout << "Αριθμός κυκλοφορίας: " << number << "-" << "Ιδιοκτήτης: " owner << "-" << "Έτος κατασκευής:" << year << endl;
+}
