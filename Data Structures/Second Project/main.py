6ea74a2baaf35606457a815e26308c20e17b71cd
Spyros Kokotos <33377581+Greekforce1821@users.noreply.github.com>
2022-05-30 15:53:19 +0300

    Add files via upload

--- Data Structures/Second Project/main.py
+++ Data Structures/Second Project/main.py
@@ -11,7 +11,7 @@ class node:
         self.left = None                    #self.left corresponds to the left part of the node
         self.right = None                   #selft.right corresponds to the right part of the node
         
-def Node():                                 #This function Node stores the data which was given by the user in a specific array called dataRecord
+def Node():                                 #This function Node stores the data which was given by the user in a specific array called dataRecord.
     r = node()
     textline = input("Give us some data:")
     dataRecord = textline.strip().split()
@@ -22,7 +22,7 @@ def Node():                                 #This function Node stores the data 
     r.ap = int(dataRecord[4])               #The variable r.ap corresponds to the fifth vavlue of the dataRecord Array
     return r
 
-def addInOrder(root,new):                   #This function addInOrder sorts the data from the array in order                     
+def addInOrder(root,new):                   #This function addInOrder sorts the data from the array in order.                    
     if new.ID <= root.ID:
         if root.left == None:
             root.left = new
@@ -34,13 +34,13 @@ def addInOrder(root,new):                   #This function addInOrder sorts the 
         else:
             addInOrder(root.right,new) 
             
-def showInOrder(root):
+def showInOrder(root):                      #This function showInOrder sorts the data from the array in order.  
     if root != None:
         showInOrder(root.left)
         print(("%s %s %s %.2f %d") % (root.ID, root.fn, root.ln, root.gr, root.ap))
         showInOrder(root.right)
 
-def countNodes(root):
+def countNodes(root):                       #This function countNodes counts the succesful results of finding the items that the user had entered earlier.
     result = 0
     
     if root  != None:
@@ -49,7 +49,7 @@ def countNodes(root):
         result += countNodes(root.right)
     return result            
      
-def findByID(root,fID):
+def findByID(root,fID):                      #This function findById runs a search of the entered ID by the user and tries to return it there is inside the tree.
     result = None
     
     if root != None:
@@ -82,7 +82,7 @@ def maxGrade(root):
 
 root = None
 
-print("---------- Menu ----------")
+print("---------- Menu ----------")                            #This is the GUI of the program in order to interact with the user and offer him the options that he would like to run.
 print("1. Add in Order")
 print("2. Show in Order")
 print("3. Find Student by Grade")
@@ -93,9 +93,41 @@ ch = input("Choice: ").strip()
 
 while ch != "0":
     if ch == "1":
-        temp = createNode()
+        temp = Node()
         if root == None:
             root = temp
         else:
             addInOrder(root, temp)
-
+    elif ch == "2":
+        showInOrder(root)
+        print(("Number of Nodes: %d") % (countNodes(root)))
+    elif ch == "3":
+        userID = input("Give us Your ID:")
+        temp = findByID(root,userID)
+        if temp == None:
+            print ("User with the specific ID was not found. Please enter another one!")
+        else:
+            print(("%s %s %s %.2f %d") % (temp.ID, temp.fn, temp.ln, temp.gr, temp.ap))
+            
+    elif ch == "4":
+        if root == None:
+            print("There is an Empty Tree.")
+        else:
+            temp = maxID(root)
+            print(("%s %s %s %.2f %d") % (temp.ID, temp.fn, temp.ln, temp.gr, temp.ap))
+    elif ch == "5":
+        if root == None:
+            print("There is an Empty Tree.")
+        else:
+            print(("Max Grade: %.2f") % (maxGrade(root)))
+    else:
+        print("Try again.")
+    print("---------- Menu ----------")
+    print("1. Add in Order")
+    print("2. Show in Order")
+    print("3. Find Student by Grade")
+    print ("4. Show Last Student by Grade")  
+    print("5. Show Maximum Grade")
+    print ("0. Exit")  
+    ch = input("Choice: ").strip()
+            
